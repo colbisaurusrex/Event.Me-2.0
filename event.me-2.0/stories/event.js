206bedc606dac32d10dbb708deecf4bae0a203ef
Colby Barthelmess <colbybarthelmess@waypointbuilding.com>
2017-08-23 20:02:56 -0700

    create user and event stories

--- event.me-2.0/stories/event.js
+++ event.me-2.0/stories/event.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { truncate } from 'lodash'
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { withKnobs, text, number, boolean } from '@storybook/addon-knobs'
+import { Card, Icon, Image, Divider, Button } from 'semantic-ui-react'
+import UserCard from '../src/UserCard.jsx'
+import ButtonGroup from '../src/ButtonGroup.jsx'
 import event from '../mocks/event.json'
 
-const {owner, description, attendees} = event
+const {title, owner, description, attendees} = event
 const style = {
   position: 'absolute',
   marginLeft: '250px',
@@ -12,29 +16,60 @@ const style = {
   marginBottom: '100px',
   fontFamily: 'Roboto Mono',
 }
-
+// NOTE: Different sections of the event card need to be their own components
 storiesOf('Event', module)
+  .addDecorator(withKnobs)
   .add('Event Card', () => {
     return (
       <div style={style}>
-        <Card style={{padding: '5px'}}>
-          <Card.Content>
-            <Image src='https://bugster.forgerock.org/jira/secure/useravatar?size=xsmall&avatarId=12840' />
-            <Card.Header style={{textAlign: 'center', marginTop: '15px'}}>
-              Host: {owner.firstName + ' ' + owner.lastName}
-            </Card.Header>
-            <Card.Meta style={{textAlign: 'center'}}>
-              <span>
-                Email: {owner.email}
-              </span>
-            </Card.Meta>
-            <Card.Description>
-              Synth jianbing gastropub raw denim bushwick godard twee, thundercats VHS swag shabby chic kickstarter subway tile put a bird on it narwhal. Succulents poutine mlkshk before they sold out chicharrones. Meh pickled lyft, franzen scenester street art adaptogen blog. Lo-fi you probably haven't heard of them kickstarter photo booth synth ramps glossier. Quinoa iceland paleo keffiyeh. Seitan viral sartorial tattooed kombucha mlkshk tumeric pok pok plaid fashion axe selfies venmo ethical fixie blue bottle. 
-            </Card.Description>
+          <div>Note: Event Cards should be of fixed height</div>
+          <Card style={{padding: '5px', minHeight: '600px'}} raised={true}>
+            <Card.Content>
+              <Image src='https://bugster.forgerock.org/jira/secure/useravatar?size=xsmall&avatarId=12840' />
+               <UserCard 
+                  name={text('Name', `${owner.firstName + ' ' + owner.lastName}`)} 
+                  email={text('Email', `${owner.email}`)}
+                  image='https://bugs.mojang.com/secure/useravatar?size=xsmall&avatarId=18936' 
+                  isHost={boolean('Is host?', false)}/>
+              <Card.Meta style={{marginTop: '15px'}}>
+                <span>
+                  Quick Summary
+                </span>
+              </Card.Meta>
+              <Card.Description>
+                {text('Event Description', `${truncate("Synth jianbing gastropub raw denim bushwick godard twee, thundercats VHS swag shabby chic kickstarter subway tile put a bird on it narwhal. Succulents poutine mlkshk before they sold out chicharrones. Meh pickled lyft, franzen scenester street art adaptogen blog. Lo-fi you probably haven't heard of them kickstarter photo booth synth ramps glossier. Quinoa iceland paleo keffiyeh. Seitan viral sartorial tattooed kombucha mlkshk tumeric pok pok plaid fashion axe selfies venmo ethical fixie blue bottle.", {'length': 150, 'separator': ' '})}`)}
+              </Card.Description>
               <div style={{marginTop: '10px', fontSize: '15px'}}>
                 <Icon name='user' />
-                {attendees.length} attending
+                {number('Attendees', `${attendees.length}`)} attending
               </div>
+              <div style={{marginTop: '10px'}}>
+                <ButtonGroup 
+                  buttonOne={'attend'}
+                  buttonTwo={'unattend'}
+                  callbackOne={() => console.log('attend event')}
+                  callBackTwo={() => console.log('unattend event')}
+                />
+              </div>
+            </Card.Content>
+          </Card>
+      </div>
+    )
+  })
+  .add('Event Summary Card', () => {
+    return(
+      <div style={style}>
+        <div>Note: Should these mini cards display the whole name or truncate?</div>
+        <Card>
+          <Card.Content>
+              <Image floated='right' size='mini' src='https://bugster.forgerock.org/jira/secure/useravatar?size=xsmall&avatarId=12840'/>
+              <Card.Header style={{marginBottom: '3px'}}>
+                  {text('user', `${truncate(title, {'length': 25})}`)}
+              </Card.Header>
+              <div style={{marginTop: '10px', fontSize: '15px'}}>
+                <Icon name='user' />
+                {number('Attendees', `${attendees.length}`)} attending
+            </div>
           </Card.Content>
         </Card>
       </div>
---
+++ event.me-2.0/stories/user.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text, number } from '@storybook/addon-knobs'
+import { Card, Icon, Image, Divider, Button } from 'semantic-ui-react'
+import user from '../mocks/user.json'
+
+const style = {
+    position: 'absolute',
+    marginLeft: '250px',
+    marginTop: '100px',
+    marginBottom: '100px',
+    fontFamily: 'Roboto Mono',
+}
+
+storiesOf('User', module)
+    .addDecorator(withKnobs)
+    .add('User Card', () => {
+        return (
+            <div style={style}>
+                <div style={{width: '300px'}}>Note: these are intended to be used both on the Event Card and in a modal displayed when the user clicks on attendees</div>
+                <Card>
+                    <Card.Content>
+                        <Image floated='right' size='mini' src='https://bugs.mojang.com/secure/useravatar?size=xsmall&avatarId=18936'/>
+                        <Card.Header style={{marginBottom: '3px'}}>
+                            {text('user', `${user.firstName + ' ' + user.lastName}`)}
+                        </Card.Header>
+                        <Card.Meta>
+                            {text('Email', `${user.email}`)}
+                        </Card.Meta>
+                    </Card.Content>
+                </Card>
+            </div>
+        )
+    })
